9319eee2ef3feeb2d9e3366668a4b245c4cba00c
andriyDev <andriydzikh@gmail.com>
2023-12-30 11:08:29 -0800

    Fix not returning `App` from `add_*consumable_event` extension functions.

--- src/app.rs
+++ src/app.rs
@@ -4,22 +4,22 @@ use bevy_ecs::{event::Event, system::ResMut};
 use crate::ConsumableEvents;
 
 pub trait ConsumableEventApp {
-  fn add_consumable_event<E: Event>(&mut self);
+  fn add_consumable_event<E: Event>(&mut self) -> &mut Self;
 
-  fn add_persistent_consumable_event<E: Event>(&mut self);
+  fn add_persistent_consumable_event<E: Event>(&mut self) -> &mut Self;
 }
 
 impl ConsumableEventApp for App {
-  fn add_consumable_event<E: Event>(&mut self) {
+  fn add_consumable_event<E: Event>(&mut self) -> &mut Self {
     self
       .init_resource::<ConsumableEvents<E>>()
-      .add_systems(First, clear_all_events::<E>);
+      .add_systems(First, clear_all_events::<E>)
   }
 
-  fn add_persistent_consumable_event<E: Event>(&mut self) {
+  fn add_persistent_consumable_event<E: Event>(&mut self) -> &mut Self {
     self
       .init_resource::<ConsumableEvents<E>>()
-      .add_systems(First, clear_consumed_events::<E>);
+      .add_systems(First, clear_consumed_events::<E>)
   }
 }
 
