ea0f060f201df890c11e5c59bca6d5d44ebdcc35
andriyDev <andriydzikh@gmail.com>
2024-01-01 16:52:46 -0800

    Add doc comments for everything.

--- src/app.rs
+++ src/app.rs
@@ -3,9 +3,31 @@ use bevy_ecs::{event::Event, system::ResMut};
 
 use crate::ConsumableEvents;
 
+/// Extension trait for `bevy_app::App` to automatically add consumable events.
 pub trait ConsumableEventApp {
+  /// Adds a consumable event type of `E`.
+  ///
+  /// These events are cleared at the start of each frame. Reads occuring
+  /// *before* an event writer will **never** see the events it produces.
+  ///
+  /// An example for when to use this is a click in a user interface. The click
+  /// would be produced in PreUpdate, and once a system consumes the click, no
+  /// other system can read that click. If a click gets to the end of the frame
+  /// without being consumed, no UI elements need it, so the event should be
+  /// discarded.
   fn add_consumable_event<E: Event>(&mut self) -> &mut Self;
 
+  /// Adds a "persistent" consumable event type of `E`.
+  ///
+  /// Only consumed events are cleared at the start of each frame. This allows
+  /// users to consume events whenever they want, even several frames after the
+  /// event was triggered. However this can cause the events to grow
+  /// indefinitely if events are not consumed.
+  ///
+  /// An example for when to use this is a line of customers. Customers can join
+  /// the line at any time, but they can only be served at 1 customer per
+  /// second. One system would write customer events at a random rate, and
+  /// another system would consume an event whenever a timer goes off.
   fn add_persistent_consumable_event<E: Event>(&mut self) -> &mut Self;
 }
 
@@ -23,10 +45,12 @@ impl ConsumableEventApp for App {
   }
 }
 
+/// A system for clearing all events of type `E`.
 fn clear_all_events<E: Event>(mut events: ResMut<ConsumableEvents<E>>) {
   events.clear();
 }
 
+/// A system for clearing just the consumed events of type `E`.
 fn clear_consumed_events<E: Event>(mut events: ResMut<ConsumableEvents<E>>) {
   events.clear_consumed();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -13,14 +13,69 @@ mod app;
 #[cfg(feature = "bevy_app")]
 pub use app::ConsumableEventApp;
 
+/// A collection of events that can be consumed.
+/// Events can be written by using a [`ConsumableEventWriter`] and are typically
+/// read using a [`ConsumableEventReader`].
+///
+/// Events can be read many times, but only "consumed" once. See
+/// [`ConsumableEventReader`] for details.
+///
+/// Generally, all systems using `ConsumableEvents` should be stricly
+/// [ordered](https://github.com/bevyengine/bevy/blob/main/examples/ecs/ecs_guide.rs).
+/// This ensures that events are consumed in a consistent order.
+///
+/// Events will remain readable in this collection until they are consumed or
+/// [`ConsumableEvents::clear`] is called. Events that have been consumed will
+/// only be fully removed once [`ConsumableEvents::clear_consumed`] or
+/// [`ConsumableEvents::clear`] is called.
+///
+/// Typically, `ConsumableEvents` are initialized automatically using the
+/// [`ConsumableEventApp`] extension trait.
+///
+/// # Example
+/// ```rust
+/// use bevy_ecs::event::Event;
+/// use bevy_consumable_event::ConsumableEvents;
+///
+/// #[derive(Event)]
+/// struct MyEvent {
+///     value: usize,
+/// }
+///
+/// // setup
+/// let mut events = ConsumableEvents::<MyEvent>::default();
+///
+/// // somewhere else: send an event
+/// events.send(MyEvent { value: 1 });
+///
+/// // somewhere else: read the events, and even mutate them.
+/// for mut event in events.read() {
+///   assert_eq!(event.value, 1);
+///   event.value = 2;
+/// }
+///
+/// // somewhere else: consume the event
+/// for mut event in events.read() {
+///   assert_eq!(event.value, 2);
+///   event.consume();
+/// }
+///
+/// // somewhere else: read the events (nothing left).
+/// assert_eq!(events.read().count(), 0);
+/// ```
 #[derive(Resource)]
 pub struct ConsumableEvents<E: Event> {
+  /// The events in the buffer. Some of these events may have already been
+  /// consumed.
   events: Vec<EventInstance<E>>,
 }
 
+/// A single event in the buffer.
 #[derive(Debug)]
 struct EventInstance<E> {
+  /// The actual event.
   event: E,
+  /// Whether the event has already been consumed.
   consumed: bool,
 }
 
@@ -32,14 +87,20 @@ impl<E: Event> Default for ConsumableEvents<E> {
 }
 
 impl<E: Event> ConsumableEvents<E> {
+  /// "Sends" `event` by writing it to the buffer. [`read`] can then read the
+  /// event.
   pub fn send(&mut self, event: E) {
     self.events.push(EventInstance { event, consumed: false });
   }
 
+  /// Sends a list of `events` all at once, which can later be [`read`]. This is
+  /// more efficient than sending each event individually.
   pub fn send_batch(&mut self, events: impl IntoIterator<Item = E>) {
     self.extend(events);
   }
 
+  /// Sends the default value of the event. Useful when the event is an empty
+  /// struct.
   pub fn send_default(&mut self)
   where
     E: Default,
@@ -47,14 +108,19 @@ impl<E: Event> ConsumableEvents<E> {
     self.send(Default::default())
   }
 
+  /// Reads the unconsumed events stored in self.
   pub fn read(&mut self) -> ConsumableEventIterator<E> {
     ConsumableEventIterator { iter: self.events.iter_mut() }
   }
 
+  /// Clears all events stored in self. Unconsumed events are also dropped.
   pub fn clear(&mut self) {
     self.events.clear();
   }
 
+  /// Clears only consumed events stored in self. This is not strictly required,
+  /// but calling it regularly will reduce memory usage (since the consumed
+  /// events cannot be read anyway).
   pub fn clear_consumed(&mut self) {
     self.events.retain(|event| !event.consumed);
   }
@@ -71,8 +137,11 @@ impl<E: Event> Extend<E> for ConsumableEvents<E> {
   }
 }
 
+/// Mutable borrow of a consumable event.
 pub struct Consume<'events, E> {
+  /// The event itself.
   event: &'events mut E,
+  /// The consumed flag.
   consumed: &'events mut bool,
 }
 
@@ -91,25 +160,54 @@ impl<'events, E> DerefMut for Consume<'events, E> {
 }
 
 impl<'events, E> Consume<'events, E> {
+  /// "Consumes" the event. "Consumed" simply means that other readers will be
+  /// unable to read (or consume) the event. This function does **not** take
+  /// ownership of the event value itself (although this can be done using
+  /// something like `swap`).
   pub fn consume(&mut self) {
     *self.consumed = true;
   }
 }
 
+/// Sends consumable events of type `E`.
+///
+/// # Usage
+///
+/// `ConsumableEventWriter`s are usually declared as a [`SystemParam`].
+/// ```
+/// use bevy_ecs::prelude::*;
+/// use bevy_consumable_event::ConsumableEventWriter;
+///
+/// #[derive(Event)]
+/// pub struct MyEvent; // Custom event type.
+///
+/// fn my_system(mut writer: ConsumableEventWriter<MyEvent>) {
+///   writer.send(MyEvent);
+/// }
+///
+/// bevy_ecs::system::assert_is_system(my_system);
+/// ```
 #[derive(SystemParam)]
 pub struct ConsumableEventWriter<'w, E: Event> {
+  /// The events to write to.
   events: ResMut<'w, ConsumableEvents<E>>,
 }
 
 impl<'w, E: Event> ConsumableEventWriter<'w, E> {
+  /// "Sends" `event` by writing it to the buffer. [`ConsumableEventReader`] can
+  /// then read the event.
   pub fn send(&mut self, event: E) {
     self.events.send(event);
   }
 
+  /// Sends a list of `events` all at once, which can later be [`read`]. This is
+  /// more efficient than sending each event individually.
   pub fn send_batch(&mut self, events: impl IntoIterator<Item = E>) {
     self.events.send_batch(events);
   }
 
+  /// Sends the default value of the event. Useful when the event is an empty
+  /// struct.
   pub fn send_default(&mut self)
   where
     E: Default,
@@ -118,19 +216,44 @@ impl<'w, E: Event> ConsumableEventWriter<'w, E> {
   }
 }
 
+/// Reads (and possibly consumes) events of type `E`.
+///
+/// # Usage
+///
+/// `ConsumableEventReader`s are usually declared as a [`SystemParam`].
+/// ```
+/// use bevy_ecs::prelude::*;
+/// use bevy_consumable_event::ConsumableEventReader;
+///
+/// #[derive(Event, Debug)]
+/// pub struct MyEvent; // Custom event type.
+///
+/// fn my_system(mut reader: ConsumableEventReader<MyEvent>) {
+///   for mut event in reader.read() {
+///     println!("{:?}", *event);
+///     event.consume();
+///   }
+/// }
+///
+/// bevy_ecs::system::assert_is_system(my_system);
+/// ```
 #[derive(SystemParam)]
 pub struct ConsumableEventReader<'w, E: Event> {
+  /// The events to read from.
   events: ResMut<'w, ConsumableEvents<E>>,
 }
 
 impl<'w, E: Event> ConsumableEventReader<'w, E> {
+  /// Reads the unconsumed events.
   pub fn read(&mut self) -> ConsumableEventIterator<E> {
     self.events.read()
   }
 }
 
+/// An iterator over the unconsumed events.
 #[derive(Debug)]
 pub struct ConsumableEventIterator<'w, E: Event> {
+  /// The iterator being wrapped.
   iter: IterMut<'w, EventInstance<E>>,
 }
 
